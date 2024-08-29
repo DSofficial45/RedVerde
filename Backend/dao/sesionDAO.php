8db16d285b5654ce000949d6c92916c5632ad4c8
DESKTOP-BKD77DO\ADMIN <Dremyon@gmail.com>
2024-08-29 15:23:37 -0300

    .

--- Backend/dao/sesionDAO.php
+++ Backend/dao/sesionDAO.php
@@ -39,7 +39,7 @@ class SesionModel {
             //verificar datos
             //obtener datos
 
-            $sesion = new SesionDAO($email, $nombre, $apellido, $telefono, $password, $isAdmin);
+            $sesion = new Perfil($email, $nombre, $apellido, $telefono, $password, $isAdmin);
             $_SESSION['sesion'] = $sesion;
 
         }

4bbbfb602a7f262c384088c9ec3dab430353fa6c
Tu Nombre <tu_correo@example.com>
2024-10-31 14:24:16 -0300

    .

---
+++ FrontEnd/Page/ADMIN/Productos/Productos.js
@@ -0,0 +1,65 @@
+import ProductosDAO from "../../dao/productoDAO.js";
+import productos from "../../dao/ProductoDao.js";
+
+let nombreFiltro = "";
+let precioFiltro = "";
+let allProductos = [];
+
+window.onload = async () => {
+    let productos = await obtenerProductos();
+    allProductos = productos;
+    mostrarProductos(productos);
+    agregarEventosFiltro();
+    agregarEvento();
+}
+
+async function obtenerProductos() {
+    let respuesta = await new ProductosDAO().obtenerProductos();
+    return respuesta.datos;
+}
+
+function mostrarProductos(productos) {
+    let datosElement = document.querySelector("#datos");
+    datosElement.innerHTML = "";
+    console.log(productos);
+    productos.forEach(producto => {
+        datosElement.innerHTML += `
+        <div class="producto">
+        <img src="${producto.imagen != null ? producto.imagen : "../../image/banner1.jpg"}" alt="${producto.nombre}" class="producto-imagen">
+        <div class="producto-info">
+            <p>Nombre: ${producto.nombre}</p>
+            <p>Precio: $${producto.precio}</p>
+            <p>Stock: ${producto.stock}</p>
+            <button class="btn-add">Agregar</button>
+        </div>
+    </div>
+    `;
+    });
+}
+
+function agregarEventosFiltro() {
+    let inputNombre = document.querySelector("#filtroNombre");
+    let inputPrecio = document.querySelector("#filtroPrecio");
+
+    inputNombre.onkeyup = () => {
+        nombreFiltro = inputNombre.value.toLowerCase();
+        filtrarProductos();
+    }
+    
+    inputPrecio.oninput = () => {
+        precioFiltro = parseFloat(inputPrecio.value);
+        document.querySelector("#precioValor").textContent = $${precioFiltro}; // Actualiza el texto que muestra el valor
+        filtrarProductos();
+    }
+}
+
+function filtrarProductos() {
+    let productosFiltrados = allProductos.filter(producto => {
+        let nombreCoincide = producto.nombre.toLowerCase().startsWith(nombreFiltro);
+        let precioCoincide = producto.precio <= precioFiltro;
+
+        return nombreCoincide && precioCoincide; 
+    });
+
+    mostrarProductos(productosFiltrados); 
+}
---
+++ FrontEnd/dao/productoDAO.js
@@ -0,0 +1,47 @@
+import Origen from "./Origen.js";
+
+export default class productoDAO{
+   
+    async verProducto(){
+        let url = Origen+"/Backend/controller/productosController.php?funcion=ver";
+        let respuestaConsulta = await fetch(url, config);
+        let respuesta = await respuestaConsulta.json();
+        return respuesta;
+    }
+
+    async eliminarProducto(){
+        let url = Origen+"/Backend/controller/productosController.php?funcion=eliminar";
+        let respuestaConsulta = await fetch(url, config);
+        let respuesta = await respuestaConsulta.json();
+        return respuesta;
+    }
+
+    async agregarProducto(){
+        let url = Origen+"/Backend/controller/productosController.php?funcion=agregar";
+        let fromData = new FormData();
+        formData.append("nombre", nombre);
+        formData.append("descripcion", descripcion);
+        formData.append("precio", precio);
+        formData.append("oferta", oferta);
+        formData.append("pImagen", imagen);
+        formData.append("stock", stock);
+
+
+        let config = {
+
+            method: "POST",
+            body:formData
+        }
+
+       let respuestaConsulta = await fetch(url, config);
+       let respuesta = await respuestaConsulta.json();
+       return respuesta;
+    }
+
+    async obtenerProducto(){
+       let url = Origen+"/Backend/controller/productosController.php?funcion=obtener";
+       let respuestaConsulta = await fetch(url, config);
+       let respuesta = await respuestaConsulta.json();
+       return respuesta;
+    }
+}

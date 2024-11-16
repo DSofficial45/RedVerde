fec08106904382f62b650fbe603f1fefe5c9ab40
Tu Nombre <tu_correo@example.com>
2024-11-15 22:52:55 -0300

    .

--- bd/redverdebd.sql
+++ bd/redverdebd.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 13, 2024 at 08:54 PM
+-- Generation Time: Nov 16, 2024 at 02:51 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -136,17 +136,41 @@ CREATE TABLE `producto` (
   `descripcion` varchar(250) NOT NULL,
   `nombreCategoria` varchar(45) NOT NULL,
   `nombre` varchar(50) NOT NULL,
-  `extension` varchar(4) NOT NULL
+  `extension` varchar(4) NOT NULL,
+  `oferta` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `producto`
 --
 
-INSERT INTO `producto` (`id`, `fecha`, `precio`, `stock`, `descripcion`, `nombreCategoria`, `nombre`, `extension`) VALUES
-(11, '0000-00-00 00:00:00', 2000, 10, 'El real GOAT del manga (y anime, solo que aun no lo animan)', 'Árboles', 'Seiichi Samura', 'png'),
-(12, '0000-00-00 00:00:00', 1000, 5, 'PROCEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEED!!!', 'Macetas', 'Kyora Sazanami', 'png'),
-(14, '0000-00-00 00:00:00', 500, 1, 'Lee Kagurabachi', 'Inciensos', 'Lee Kagurabachi', 'png');
+INSERT INTO `producto` (`id`, `fecha`, `precio`, `stock`, `descripcion`, `nombreCategoria`, `nombre`, `extension`, `oferta`) VALUES
+(26, '0000-00-00 00:00:00', 18990, 25, 'Descripcion de Arbol 1', 'Árboles', 'Arbol 1', 'webp', 0),
+(27, '0000-00-00 00:00:00', 18990, 30, 'Descripcion de Arbol 2', 'Árboles', 'Arbol 2', 'webp', 20),
+(28, '0000-00-00 00:00:00', 18990, 35, 'Descripcion de Arbol 3', 'Árboles', 'Arbol 3', 'png', 40),
+(29, '0000-00-00 00:00:00', 18990, 40, 'Descripcion de Arbol 4', 'Árboles', 'Arbol 4', 'png', 60),
+(32, '0000-00-00 00:00:00', 18990, 45, 'Descripcion de Arbol 5', 'Árboles', 'Arbol 5', 'png', 70),
+(33, '0000-00-00 00:00:00', 18990, 50, 'Descripcion de Arbol 6', 'Árboles', 'Arbol 6', 'webp', 80),
+(34, '0000-00-00 00:00:00', 18990, 55, 'Descripcion de Arbol 7', 'Árboles', 'Arbol 7', 'webp', 90),
+(35, '0000-00-00 00:00:00', 18990, 25, 'Descripcion de Arbol 8', 'Árboles', 'Arbol 8', 'webp', 25),
+(36, '0000-00-00 00:00:00', 18990, 35, 'Descripcion de Arbol 9', 'Árboles', 'Arbol 9', 'webp', 30),
+(37, '0000-00-00 00:00:00', 18990, 35, 'Descripcion de Arbol 10', 'Árboles', 'Arbol 10', 'png', 45),
+(38, '0000-00-00 00:00:00', 18990, 40, 'Descripcion de Arbol 11', 'Árboles', 'Arbol 11', 'webp', 55),
+(39, '0000-00-00 00:00:00', 4275, 100, 'Descripcion de Planta 1', 'Florales', 'Planta 1', 'png', 0),
+(40, '0000-00-00 00:00:00', 4275, 105, 'Descripcion de Planta 2', 'Florales', 'Planta 2', 'png', 20),
+(41, '0000-00-00 00:00:00', 4275, 110, 'Descripcion de Planta 3', 'Florales', 'Planta 3', 'png', 30),
+(42, '0000-00-00 00:00:00', 4275, 115, 'Descripcion de Planta 4', 'Macetas', 'Planta 4', 'png', 40),
+(43, '0000-00-00 00:00:00', 4275, 120, 'Descripcion de Planta 5', 'Florales', 'Planta 5', 'png', 50),
+(44, '0000-00-00 00:00:00', 4275, 125, 'Descripcion de Planta 6', 'Florales', 'Planta 6', 'png', 60),
+(45, '0000-00-00 00:00:00', 4275, 130, 'Descripcion de Planta 7', 'Macetas', 'Planta 7', 'png', 70),
+(46, '0000-00-00 00:00:00', 4275, 135, 'Descripcion de Planta 8', 'Florales', 'Planta 8', 'png', 85),
+(47, '0000-00-00 00:00:00', 4275, 140, 'Descripcion de Planta 9', 'Florales', 'Planta 9', 'png', 75),
+(49, '0000-00-00 00:00:00', 4275, 145, 'Descripcion de planta 10', 'Florales', 'Planta 10', 'png', 90),
+(50, '0000-00-00 00:00:00', 3060, 200, 'Descripcion de Arbusto 1', 'Arbustos', 'Arbusto 1', 'png', 0),
+(51, '0000-00-00 00:00:00', 3060, 205, 'Descripcion de Arbusto 2', 'Arbustos', 'Arbusto 2', 'png', 10),
+(52, '0000-00-00 00:00:00', 3060, 210, 'Descripcion de Arbusto 3', 'Arbustos', 'Arbusto 3', 'png', 20),
+(53, '0000-00-00 00:00:00', 3060, 215, 'Descripcion de Arbusto 4', 'Arbustos', 'Arbusto 4', 'png', 10),
+(54, '0000-00-00 00:00:00', 3060, 220, 'Descripcion de Arbusto 5', 'Arbustos', 'Arbusto 5', 'webp', 30);
 
 -- --------------------------------------------------------
 
@@ -288,7 +312,7 @@ ALTER TABLE `oferta`
 -- AUTO_INCREMENT for table `producto`
 --
 ALTER TABLE `producto`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=55;
 
 --
 -- Constraints for dumped tables

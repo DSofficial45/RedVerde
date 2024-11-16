ad6c785f82c09a8a874bfdab9456428512cffb75
Tu Nombre <tu_correo@example.com>
2024-11-16 08:30:51 -0300

    .

--- FrontEnd/Page/ADMIN/Productos/Productos.js
+++ FrontEnd/Page/ADMIN/Productos/Productos.js
@@ -1,11 +1,5 @@
 import productoDAO from "../../../dao/productoDAO.js";
 
-/*let nombreFiltro = "";
-let precioFiltro = "";
-let allProductos = [];*/
-
-let productos = []; // Declarar productos como variable global
-
 window.onload = async () => {
     let productos = await obtenerProductos();
     //allProductos = productos;
@@ -19,36 +13,34 @@ async function obtenerProductos() {
     return respuesta.datos;
 }
 
-function mostrarProductos(productos) {
+function mostrarProductos(productos) { 
     let datosElement = document.querySelector("#datos");
     datosElement.innerHTML = "";
     console.log(productos);
-
+     
     productos.forEach(producto => {
-        let ofertaTexto = "Sin oferta";
-        if (producto.oferta && producto.oferta > 0) {
-            ofertaTexto = `${producto.oferta}%`;
-        }
-
-        let filaHTML = `
-        <tr>
+    let ofertaTexto = "Sin oferta";
+     if (producto.oferta && producto.oferta > 0) {
+        ofertaTexto = `${producto.oferta}%`; }
+        
+    let filaHTML = `
+        <tr> 
             <td><img src="${producto.urlImg}" alt="Producto ${producto.nombre}" width="100px" height="100px"></td>
-            <td>${producto.nombre}</td>
-            <td>${producto.fecha}</td>
-            <td>$${producto.precio}</td>
+            <td>${producto.nombre}</td> 
+            <td>${producto.fecha}</td> 
+            <td>$${producto.precio}</td> 
             <td>${ofertaTexto}</td>
-            <td>${producto.stock}</td>
-            <td class="descripcion">${producto.descripcion}</td>
-            <td>${producto.nombreCategoria}</td>
+            <td>${producto.stock}</td> 
+            <td class="descripcion">${producto.descripcion}</td> 
+            <td>${producto.categoria}</td>
             <td><button class="botonAccion">Modificar</button></td>
             <td><button class="botonAccion">Eliminar</button></td>
-        </tr>
-        `;
-
-        datosElement.innerHTML += filaHTML;
+        </tr> `; 
+        
+        datosElement.innerHTML += filaHTML; 
     });
 }
-
+    
 function agregarEvento(){
     let formElement = document.querySelector("#formularioProducto");
     formElement.onsubmit = (e)=>{
@@ -63,6 +55,7 @@ function agregarEvento(){
 
         agregarProducto(nombre, descripcion, precio, categoria, oferta, imagen, stock);
     }
+
 }
 
 async function agregarProducto(nombre, descripcion, precio, categoria, oferta, imagen, stock) {
@@ -71,3 +64,40 @@ async function agregarProducto(nombre, descripcion, precio, categoria, oferta, i
     mostrarProductos(productos);
   
 }
+
+/*function agregarEventoFiltro() { document.querySelector("#filtroSelect").addEventListener("change", function() { filtrarProductos(this.value); }); }
+
+function filtrarProductos(filtro) {
+    let productosFiltrados = [...productos];
+
+    switch (filtro) {
+        case "stockAsc":
+            productosFiltrados.sort((a, b) => a.stock - b.stock);
+            break;
+        case "stockDesc":
+            productosFiltrados.sort((a, b) => b.stock - a.stock);
+            break;
+        case "precioAsc":
+            productosFiltrados.sort((a, b) => a.precio - b.precio);
+            break;
+        case "precioDesc":
+            productosFiltrados.sort((a, b) => b.precio - a.precio);
+            break;
+        case "ventasAsc":
+            productosFiltrados.sort((a, b) => a.ventas - b.ventas);
+            break;
+        case "ventasDesc":
+            productosFiltrados.sort((a, b) => b.ventas - a.ventas);
+            break;
+        case "ofertaAsc":
+            productosFiltrados.sort((a, b) => a.oferta - b.oferta);
+            break;
+        case "ofertaDesc":
+            productosFiltrados.sort((a, b) => b.oferta - a.oferta);
+            break;
+        default:
+            break;
+    }
+
+    mostrarProductos(productosFiltrados);
+}*/
